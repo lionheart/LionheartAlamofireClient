c296bbcfb6fd60e15611cbe412c996b620a296ef
Dan Loewenherz <dloewenherz@gmail.com>
2016-03-20 17:55:02 -0500

    add supporting scripts

---
+++ generate_docs.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+jazzy \
+  --author Lionheart \
+  --author_url http://lionheartsw.com \
+  --github_url https://github.com/lionheart/LionheartAlamofireClient \
+  --github-file-prefix https://github.com/lionheart/LionheartAlamofireClient/tree/1.0.0 \
+  --module LionheartAlamofireClient
+
+git co gh-pages
+cp -r docs/* .
+rm -rf docs/
+git add .
+git commit -m "documentation update"
+git co master
