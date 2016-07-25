739ff952bcfd49783b2a21c0b7edcc948984d8ba
Dan Loewenherz <dan@lionheartsw.com>
2016-07-25 15:00:33 -0500

    bump version to 1.3.5

--- generate_docs.sh
+++ generate_docs.sh
@@ -4,7 +4,7 @@ jazzy \
   --author Lionheart \
   --author_url http://lionheartsw.com \
   --github_url https://github.com/lionheart/LionheartAlamofireClient \
-  --github-file-prefix https://github.com/lionheart/LionheartAlamofireClient/tree/1.3.4 \
+  --github-file-prefix https://github.com/lionheart/LionheartAlamofireClient/tree/1.3.5 \
   --module LionheartAlamofireClient
 
 git co gh-pages
